1fabddd2eed35a4b6bd1c925f589c5a436c8f85f
LUEZ117 <927068525@qq.com>
2021-07-21 11:50:08 +0800

    Update datasets.py

--- utils/datasets.py
+++ utils/datasets.py
@@ -63,8 +63,9 @@ def exif_size(img):
 def create_dataloader(path, imgsz, batch_size, stride, opt, hyp=None, augment=False, cache=False, pad=0.0, rect=False,
                       rank=-1, world_size=1, workers=8, image_weights=False, quad=False, prefix=''):
     # Make sure only the first process in DDP process the dataset first, and the following others can use the cache
-    with torch_distributed_zero_first(rank):
-        dataset = LoadImagesAndLabels(path, imgsz, batch_size,
+    
+    # with torch_distributed_zero_first(rank):
+    dataset = LoadImagesAndLabels(path, imgsz, batch_size,
                                       augment=augment,  # augment images
                                       hyp=hyp,  # augmentation hyperparameters
                                       rect=rect,  # rectangular training
